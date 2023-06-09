460974843dd3011ee511e59e4925094d73a746aa
Alfatista2 <teteusoliveira9@gmail.com>
2023-06-09 18:53:21 -0300

    V2.0.0 - second screen and theme picker

--- script.js
+++ script.js
@@ -1,90 +1,120 @@
 let participants = document.querySelector(".participants");
 let main = document.querySelector("main");
-
-// function createNewParticipant(buttonTrue = true) {
-//   let li = document.createElement("li");
-
-//   let input = document.createElement("input");
-//   input.type = "text";
-//   input.placeholder = "Participante";
-//   li.appendChild(input);
-
-//   if (buttonTrue) {
-//     let button = document.createElement("input");
-//     button.type = "button";
-//     button.value = "🚫";
-//     button.placeholder = "participante";
-//     li.appendChild(button);
-//     button.addEventListener("click", function () {
-//       li.remove();
-//     });
-//   } else {
-//     input.required = true;
-//   }
-
-//   participants.appendChild(li);
-// }
-
-// createNewParticipant(false);
-// createNewParticipant(false);
-
-// const form = document.querySelector("form");
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   let rounds = +document.querySelector("#rounds").value;
-
-//   let arrayParticipants = [];
-//   for (let i = 0; i < +participants.childElementCount; ++i) {
-//     arrayParticipants.sort(() => Math.random() - 0.5);
-//     let input = participants.children[i].firstChild.value;
-//     input !== "" && arrayParticipants.push(input);
-//   }
-//   arrayParticipants.sort(() => Math.random() - 0.5);
-//   let arrayPaired = combineParticipants(arrayParticipants);
-//   console.log(arrayPaired);
-//   let texto = "";
-//   for (let i = 0; i < arrayPaired.length; ++i) {
-//     texto += arrayPaired[i].length > 1 ? `<p>${arrayPaired[i][0]} vs ${arrayPaired[i][1]}</p>` : `<p>BYE: ${arrayPaired[i][0]}</p>`;
-//   }
-//   main.innerHTML = texto;
-//   main.style.justifyContent = "center";
-// });
-
-// function combineParticipants(array) {
-//   const newArray = [];
-
-//   for (let i = 0; i < array.length; i += 2) {
-//     const firstElement = array[i];
-//     const secondElement = array[i + 1];
-
-//     if (firstElement !== undefined && secondElement !== undefined) {
-//       newArray.push([firstElement, secondElement]);
-//     } else if (firstElement !== undefined) {
-//       newArray.push(firstElement);
-//     }
-//   }
-
-//   return newArray;
-// }
-
-let themes = ["", "dark_mode", "purple_mode"];
-let counter = 0;
 let body = document.querySelector("body");
-console.log(body);
-document.onkeyup = function (event) {
-  console.log("hey, funfou!");
-  if (event.key === "AltGraph" && event.ctrlKey) {
-    console.log("ei, funfou");
-    if (counter === themes.length - 1) {
-      counter = 0;
-      console.log("bruh");
-      body.setAttribute("class", themes[counter]);
-      console.log("estou aqui 1");
-    } else {
-      ++counter;
-      console.log("bruh");
-      body.setAttribute("class", themes[counter]);
-      console.log("estou aqui 2");
+let first_screen = document.querySelector(".first_screen");
+
+function createNewParticipant(buttonTrue = true) {
+  let li = document.createElement("li");
+
+  let input = document.createElement("input");
+  input.type = "text";
+  input.placeholder = "Participante";
+  li.appendChild(input);
+
+  if (buttonTrue) {
+    let button = document.createElement("button");
+    button.type = "button";
+    button.innerText = "❌";
+    button.tabIndex = -1;
+    li.appendChild(button);
+    button.addEventListener("click", function () {
+      li.remove();
+    });
+  } else {
+    input.required = true;
+  }
+
+  participants.appendChild(li);
+}
+
+createNewParticipant(false);
+createNewParticipant(false);
+
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let rounds = +document.querySelector("#rounds").value;
+
+  let arrayParticipants = [];
+  for (let i = 0; i < +participants.childElementCount; ++i) {
+    arrayParticipants.sort(() => Math.random() - 0.5);
+    let input = participants.children[i].firstChild.value;
+    input !== "" && arrayParticipants.push(input);
+  }
+  arrayParticipants.sort(() => Math.random() - 0.5);
+
+  secondScreen(arrayParticipants);
+});
+
+let versus = document.querySelector("#versus");
+let second_screen = document.querySelector(".second_screen");
+let table = document.querySelector("#table");
+
+class Player {
+  tablePosition;
+  j;
+  v;
+  d;
+  bye;
+
+  constructor(name) {
+    this.name = name;
+  }
+}
+function secondScreen(array = []) {
+  first_screen.style.display = "none";
+  second_screen.style.display = "flex";
+  console.log(array);
+  let bye = array.length % 2;
+  let texto = "";
+  for (let i = 0; i < array.length - 1; i += 2) {
+    texto += `<li>${array[i]} <input type="number" min="0" required /> vs <input type="number" min="0" required /> ${array[i + 1]}</li>`;
+  }
+  bye ? (texto += `<li>Bye: <span class="bye">${array[array.length - 1]}</span></li>`) : null;
+
+  let tableCell = [];
+  let tableHead = table.querySelector("thead tr");
+  let tableBody = table.querySelector("tbody");
+  let arraySorted = array.sort();
+  for (let i = 0; i < arraySorted.length; i++) {
+    tableCell.push(`<tr><td>${arraySorted[i]}</td>
+    <td>0</td>
+    <td>0</td>
+    <td>0</td>
+    <td>0</td>
+    <td>0</td>
+  </tr>`);
+  }
+  versus.innerHTML = texto;
+  tableBody.innerHTML = tableCell.join("");
+}
+
+function combineParticipants(array) {
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i += 2) {
+    const firstElement = array[i];
+    const secondElement = array[i + 1];
+
+    if (firstElement !== undefined && secondElement !== undefined) {
+      newArray.push([firstElement, secondElement]);
+    } else if (firstElement !== undefined) {
+      newArray.push(firstElement);
     }
   }
+
+  return newArray;
+}
+
+document.onkeyup = function (event) {
+  if (event.key === "AltGraph" && event.ctrlKey) {
+    trocaCor();
+  }
 };
+
+let themes = ["", "orange_mode", "dark_mode", "purple_mode"];
+let counter = 0;
+function trocaCor() {
+  let botao = document.querySelector(".theme-circle:checked");
+  botao.nextElementSibling ? (botao.nextElementSibling.checked = true) : (document.querySelector(".theme-circle").checked = true);
+}
--- teste.js
+++ teste.js
@@ -10,3 +10,47 @@ function comparar(a, b) {
   return a[2] === b[2] ? b[1] - a[1] : b[2] - a[2];
 }
 console.log(jogadores);
+`
+<section class="second_screen">
+        <form action="#" class="rounds">
+          <h2>Rodada 1</h2>
+          <ul>
+            <li>Fulano <input type="number" min="0" required /> vs <input type="number" min="0" required /> Ciclano</li>
+            <li>Fulano <input type="number" min="0" required /> vs <input type="number" min="0" required /> Ciclano</li>
+            <li>Fulano <input type="number" min="0" required /> vs <input type="number" min="0" required /> Ciclano</li>
+            <li>Bye: <span class="bye">beltrano</span></li>
+            <!-- Adicione mais <li> para cada rodada -->
+          </ul>
+          <button type="submit" class="advance-button">Avançar Rodada</button>
+        </form>
+        <div class="table">
+          <table>
+            <tr>
+              <th></th>
+              <th>J</th>
+              <th>V</th>
+              <th>E</th>
+              <th>D</th>
+              <th>Bye</th>
+            </tr>
+            <tr>
+              <td>Fulano</td>
+              <td>5</td>
+              <td>4</td>
+              <td>1</td>
+              <td>0</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Ciclano</td>
+              <td>4</td>
+              <td>2</td>
+              <td>2</td>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+
+            <!-- Adicione mais <tr> para cada linha da tabela -->
+          </table>
+        </div>
+      </section>`;
